71120c6c0fb1f915c7ec06bc40da80f57f029b38
shaik <shaik@TOXIN>
2024-02-12 14:37:00 +0530

    Add project files.

---
+++ improve_customer_renewl_subscription/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace improve_customer_renewl_subscription
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int dayUNtilExpiration = random.Next(12);
+            int discountPercentge = 0;
+
+            
+            if (dayUNtilExpiration == 0)
+            {
+                Console.WriteLine("Your subscription has expired.");
+            }
+            else if (dayUNtilExpiration == 1)
+            {
+                Console.WriteLine("Your subscription expires within a day! Renew now and save 20 % !");
+                discountPercentge = 20;
+            }
+            else if (dayUNtilExpiration <= 5)
+            {
+                Console.WriteLine($"Your subscription expires in {dayUNtilExpiration} days.Renew now and save 10% !");
+                discountPercentge = 10;
+            }
+            if (dayUNtilExpiration <= 10)
+            {
+                Console.WriteLine("Your subscription will expire soon. Renew now!");
+            }
+            if (discountPercentge > 0)
+            {
+                Console.WriteLine($"Renew now and save {discountPercentge}");
+            }
+
+
+            Console.ReadLine();
+            }
+        }
+    }
+
---
+++ improve_customer_renewl_subscription/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("improve_customer_renewl_subscription")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("improve_customer_renewl_subscription")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3e7336fe-9120-428f-9548-0f80e0b9bcfc")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
